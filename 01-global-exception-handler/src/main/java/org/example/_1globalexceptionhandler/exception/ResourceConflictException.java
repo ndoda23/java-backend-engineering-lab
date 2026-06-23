94419f50cc33cd30abd2b43f60f3d697c11f84ed
ndoda23 <ndoda23@freeuni.edu.ge>
2026-06-23 20:16:02 +0400

    Delete 01-global-exception-handler directory

--- 01-global-exception-handler/src/main/java/org/example/_1globalexceptionhandler/exception/ResourceConflictException.java
+++
@@ -1,7 +0,0 @@
-package org.example._1globalexceptionhandler.exception;
-
-public class ResourceConflictException extends RuntimeException {
-    public ResourceConflictException(String message) {
-        super(message);
-    }
-}
