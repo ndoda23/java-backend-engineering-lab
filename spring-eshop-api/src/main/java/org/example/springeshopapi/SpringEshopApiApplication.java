2a23c412e1b2174f5713138b4942a4ad423e9ee9
ndoda23 <ndoda23@freeuni.edu.ge>
2026-06-26 23:28:53 +0400

    add files for new project e-commerce api

---
+++ spring-eshop-api/src/main/java/org/example/springeshopapi/SpringEshopApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.springeshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEshopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEshopApiApplication.class, args);
+    }
+
+}
---
+++ spring-eshop-api/src/test/java/org/example/springeshopapi/SpringEshopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springeshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEshopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
